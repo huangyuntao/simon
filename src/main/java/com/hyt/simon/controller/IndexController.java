283721d50fded4ab7a459bc6f4d865ec2ea063b8
hyt <hyt_yuntao@163.com>
2018-08-14 18:25:14 +0800

    增加地址/ok检测项目是否正常运行

--- src/main/java/com/hyt/simon/controller/IndexController.java
+++ src/main/java/com/hyt/simon/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.hyt.simon.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -10,4 +11,11 @@ public class IndexController {
     public String  list() {
         return "index";
     }
+	
+	@RequestMapping("/ok")
+	@ResponseBody
+	public String  ok() {
+		return "ok";
+	}
+	
 }
